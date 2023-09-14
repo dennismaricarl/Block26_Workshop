2d45476db7dc0ca626fb79b4918426007b45f27b
Mari <dennismaricarl@gmail.com>
2023-09-13 19:00:19 -0700

    set up Contact List table

--- Block26_Workshop/src/App.jsx
+++ Block26_Workshop/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ContactList from './Components/ContactList'
 
 function App() {
 
   return (
     <>
+      <ContactList />
     
     </>
-  )
+  );
 }
 
 export default App
